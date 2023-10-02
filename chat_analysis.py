4c1a5dc5da40eb582c6d57ae45f7da2250c9f65b
Noaa Maman <noaamaman325158@gmail.com>
2023-10-03 02:40:07 +0300

    Create some main functionality for handle the file itself

--- chat_analysis.py
+++ chat_analysis.py
@@ -35,4 +35,28 @@ def getDataPoints(line):
         author = None
     return date, time, author, message
 
+data = []
+conversation = "ChatExample.txt"
+with open(conversation, encoding="utf-8") as fp:
+    fp.readline()
+    messageBuffer = []
+    date, time, author = None, None, None
+    while True:
+        line = fp.readline()
+        if not line:
+            break
+        line = line.strip()
+        if date_time(line):
+            if len(messageBuffer) > 0:
+                data.append([date, time, author, ' '.join(messageBuffer)])
+
+            messageBuffer.clear()
+            date, time, author, message = getDataPoints(line)
+            messageBuffer.append((message))
+        else:
+            messageBuffer.append((line))
+
+
+
+
 
