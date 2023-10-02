ad4bb4a8aa0b121d30cfa7df4ead95669b04a8b0
Noaa Maman <noaamaman325158@gmail.com>
2023-10-03 02:19:24 +0300

    Give me some example for the required format and in addition create for infrastructure for get the specific necessary data

--- chat_analysis.py
+++ chat_analysis.py
@@ -14,3 +14,25 @@ def date_time(s):
     return False
 
 
+def find_author(s):
+    s = s.split(":")
+
+    if len(s) == 2:
+        return True
+    else:
+        return False
+def getDataPoints(line):
+    splitline = line.split('-')
+
+    dateTime = splitline[0]
+    date, time = dateTime.split.split(', ')
+
+    message = " ".join(splitline[1:])
+    if find_author(message):
+        splitmessage = message.split(": ")
+        message = " ".join(splitmessage[1:])
+    else:
+        author = None
+    return date, time, author, message
+
+
